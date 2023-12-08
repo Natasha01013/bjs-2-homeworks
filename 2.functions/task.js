02c5028c2f216139e82540e492bc00a565dfcbf7
Natalia Davydova <natasha01013@yandex.ru>
2023-12-08 19:09:52 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,23 @@
 function getArrayParams(...arr) {
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        min = Math.min(min, arr[i])
+        max = Math.max(max, arr[i])
+        sum += arr[i];
+    }
+    let avg = +((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
+
+console.log(getArrayParams(-99, 99, 10));
+console.log(getArrayParams(1, 2, 3, -100, 10));  
+console.log(getArrayParams(5));
+
+
+
 
 function summElementsWorker(...arr) {
 
