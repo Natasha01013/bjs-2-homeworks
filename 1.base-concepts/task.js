8162accde2ff226651691e4be5d9c3a34cf81697
Natalia Davydova <natasha01013@yandex.ru>
2023-12-20 13:12:56 +0300

    Work is ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,19 @@
 //Задача 1
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b, 2) - (4 * a * c);
 
-  if (d < 0) {
-    return arr = [];
-  } else if (d === 0) {
-    arr.push(- b / (2 * a));
-  } else {
-  arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
-  }
-  return arr;
+function solveEquation(a, b, c) {
+   let arr = [];
+   let d = Math.pow(b, 2) - (4 * a * c);
+
+   if (d < 0) {
+      return arr;
+   } else if (d === 0) {
+      arr.push(- b / (2 * a));
+   } else {
+      arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+   }
+  
+   return arr;
 }
 
 console.log(solveEquation(5, 12, 3));
@@ -19,11 +21,11 @@ console.log(solveEquation(5, 12, 3));
 
 // Задча 2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percentBet = percent / 100; // процентная ставка 
+  let percentBet = percent / 100; // процентная ставка 
   let bodyCredit = amount - contribution; // тело кредита
   
   let monthlyFee = bodyCredit * ((percentBet / 12) + ((percentBet / 12) / (((1 + (percentBet / 12)) ** countMonths) - 1)));
-  let totalCredit = +((monthlyFee * countMonths).toFixed(2))
+  let totalCredit = +((monthlyFee * countMonths).toFixed(2));
 
   return totalCredit;
 }
