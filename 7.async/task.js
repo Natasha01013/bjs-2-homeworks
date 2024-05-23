7360e97d44d43e2b46c4d12630751368fa565617
Natalia Davydova <natasha01013@yandex.ru>
2024-05-23 18:57:27 +0300

    Initial commit

---
+++ 7.async/task.js
@@ -0,0 +1,83 @@
+class AlarmClock {
+	constructor(alarmCollection, intervalId){
+		this.alarmCollection = [];   //свойство для хранения коллекции звонков
+		this.intervalId = null;      //свойство для хранения id таймера
+	}
+
+	//добавляет новый звонок в коллекцию существующих
+	addClock(time, callback){
+		if (time === undefined || callback === undefined) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		} 
+
+		const sameTime = this.alarmCollection.find(item => item.time === time);
+		if(sameTime) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
+
+		this.alarmCollection.push({
+			callback: callback,
+			time: time,
+			canCall: true
+		});
+	}
+
+	
+	//удаляет звонки по определённому времени
+	removeClock(time){
+		this.alarmCollection = this.alarmCollection.filter(item => {
+			return item.time !== time;
+		});
+	}
+
+	
+	//возвращает текущее время в строковом формате HH:MM
+	getCurrentFormattedTime(){
+		let currentTime = new Date();
+		let hour = currentTime.getHours(); 
+		let minute = currentTime.getMinutes();
+
+		if(hour < 10) {
+			hour = '0' + hour;
+		}	
+		if(minute < 10) {
+			minute = '0' + minute;
+		}
+
+		return hour + minute;
+	}
+
+	//запускает будильник
+	start(){
+		if(this.intervalId !== undefined) {
+			return
+		} else {
+			this.intervalId = setInterval(() => {
+				this.alarmCollection.forEach((item) => {
+					if ((this.getCurrentFormattedTime() = item.time) && item.canCall) {
+						item.canCall = false;
+						item.callback();
+					} 
+				});
+			}, 5000);
+		}
+	}
+
+	//останавливает выполнение интервала будильника
+	stop(){
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	//сбрасывает возможность запуска всех звонков
+	resetAllCalls(){
+		this.alarmCollection.forEach(item => {
+			item.canCall = true;
+	});
+
+	//удаляет все звонки
+	clearAlarms(){
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
